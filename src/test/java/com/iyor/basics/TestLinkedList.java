a72ea5831c31d6b913a956788e3bac6c2680f501
Erik <erikejan@hotmail.com>
2016-12-26 23:40:30 +0100

    Tests now functional with maven (not junit)

--- src/test/java/com/iyor/basics/TestLinkedList.java
+++ src/test/java/com/iyor/basics/TestLinkedList.java
@@ -1,37 +1,47 @@
-public class TestLinkedList {
-	//TODO add JUnit testing instead...
-	public static void main(String[] args){
-		testCreation();
-		testAdd();
-		testContainsIfEmpty();
-		testContains();
-	}
+package com.iyor.basics;
 
-	public static void testCreation() {
+import com.iyor.basics.LinkedList;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestLinkedList extends TestCase {
+
+    public TestLinkedList( String testName ){
+        super( testName );
+    }
+	
+    public static Test suite(){
+        return new TestSuite( TestLinkedList.class );
+    }
+
+	public void testCreation() {
 		LinkedList<Integer> ll = new LinkedList<Integer>();
-		System.out.println("Creation successful: " + (0==ll.size()));
+		assertTrue(0==ll.size());
 	}
 
-	public static void testAdd() {
-		LinkedList<Integer> ll = new LinkedList<Integer>();
-		ll.add(1);
-		ll.add(2);
-		ll.add(3);
-		System.out.println("Addition successful: " + (ll.size==3));
-	}
-
-	public static void testContainsIfEmpty(){
-		LinkedList<Integer> ll = new LinkedList<Integer>();
-		System.out.println("Contains if empty successful: " +
-			!ll.contains(4));
-	}
-
-	public static void testContains() {
+	public void testAdd() {
 		LinkedList<Integer> ll = new LinkedList<Integer>();
 		ll.add(1);
 		ll.add(2);
 		ll.add(3);
-		System.out.println("Contains successful " 
-			+ (ll.contains(1) && ll.contains(2) && ll.contains(3) ));
+		assertTrue(3==ll.size());
+	}
+
+	public void testContainsIfEmpty(){
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		assertFalse(ll.contains(1));
+	}
+
+	public void testContains() {
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		ll.add(1);
+		ll.add(2);
+		ll.add(3);
+		assertTrue(ll.contains(1));
+		assertTrue(ll.contains(2));
+		assertTrue(ll.contains(3));
 	}
 }
+
