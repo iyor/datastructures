cfdb1a90ec94aa837d8d069a4838c2bd3213df72
Erik <erikejan@hotmail.com>
2016-12-28 12:28:04 +0100

    More stack tests

--- src/test/java/com/iyor/basics/StackTest.java
+++ src/test/java/com/iyor/basics/StackTest.java
@@ -25,4 +25,17 @@ public class StackTest {
 		assertTrue(3==st.pop());	
 		assertTrue(4==st.pop());	
 	}
+
+	@Test 
+	public void popAndPushTest() {
+		st.pop();
+		st.pop();
+		st.push(8);
+		assertTrue(8==st.pop());
+	}
+	@Test
+	public void popWhenEmpty() {
+		st = new Stack<Integer>();
+		assertNull(st.pop());
+	}
 }
