7fae571a59dcccddc2087ce030a0fad9b41326dc
Erik <erikejan@hotmail.com>
2016-12-29 21:05:36 +0100

    Traversal (pre-order) tested

--- src/test/java/com/iyor/tree/BSTreeTest.java
+++ src/test/java/com/iyor/tree/BSTreeTest.java
@@ -3,6 +3,9 @@ package com.iyor.trees;
 import org.junit.Test;
 import org.junit.Before;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class BSTreeTest {
@@ -22,7 +25,7 @@ public class BSTreeTest {
 	}
 
 	@Test
-	public void testContains() {
+	public void containsTest() {
 		assertTrue(bst.contains(4));	
 		assertTrue(bst.contains(2));	
 		assertTrue(bst.contains(1));	
@@ -30,5 +33,14 @@ public class BSTreeTest {
 		assertTrue(bst.contains(6));	
 		assertTrue(bst.contains(5));	
 		assertTrue(bst.contains(7));	
+		assertFalse(bst.contains(0));
+	}
+
+	@Test
+	public void traversePreOrderTest(){
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.addAll(Arrays.asList(4,2,1,3,6,5,7));
+		ArrayList<Integer> result = bst.traversePreOrder();
+		assertTrue(expected.equals(result));
 	}
 }
