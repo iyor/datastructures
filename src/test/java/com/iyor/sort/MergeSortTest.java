c3db26a31e676993a9586cf16d2c1cb6c690dbd2
Erik <erikejan@hotmail.com>
2017-01-05 20:45:56 +0100

    Added more incremental tests

--- src/test/java/com/iyor/sort/MergeSortTest.java
+++ src/test/java/com/iyor/sort/MergeSortTest.java
@@ -3,24 +3,32 @@ package com.iyor.sort;
 import java.util.Arrays;
 
 import org.junit.Test;
-import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 public class MergeSortTest {
 
-	Integer[] intArray;
-
-	@Before
-	public void testInit() {
-		intArray = new Integer[] {3,2,6,8,4,6,5,3,10,3,11,-2,123,123};	
+	@Test
+	public void singleElementTest() {
+		Integer[] arr = new Integer[] {2};	
+		Integer[] sorted = MergeSort.sort(arr);
+		Integer[] expected = {2};
+		assertTrue(Arrays.equals(sorted, expected));
 	}
 
 	@Test
-	public void singleElementTest() {
+	public void simpleSortTest() {
+		Integer[] arr = new Integer[] {3,2};	
+		Integer[] sorted = MergeSort.sort(arr);
+		Integer[] expected = {2,3};
+		assertTrue(Arrays.equals(sorted, expected));
+	}
+
+	@Test
+	public void oddLengthArrayTest() {
 		Integer[] arr = new Integer[] {3,2,6};	
 		Integer[] sorted = MergeSort.sort(arr);
-		Integer[] expected = {2};
+		Integer[] expected = {2,3,6};
 		assertTrue(Arrays.equals(sorted, expected));
 	}
 
