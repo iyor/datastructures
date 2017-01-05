1950e98f1d237c5edff7ca7fb1223f4a2dff9ef4
Erik <erikejan@hotmail.com>
2017-01-05 21:24:00 +0100

    All tests for mergesort now passing

--- src/test/java/com/iyor/sort/MergeSortTest.java
+++ src/test/java/com/iyor/sort/MergeSortTest.java
@@ -32,10 +32,11 @@ public class MergeSortTest {
 		assertTrue(Arrays.equals(sorted, expected));
 	}
 
-//	@Test
-//	public void mergeSortTest() {
-//		Integer[] sorted = MergeSort.sort(intArray);
-//		Integer[] expected = {-2, 2, 3, 3, 3, 4, 5, 6, 6, 8, 10, 11, 123, 123};
-//		assertTrue(Arrays.equals(sorted, expected));
-//	}
+	@Test
+	public void mergeSortTest() {
+		Integer[] arr = new Integer[] {3,2,6,8,4,6,5,3,10,3,11,-2,123,123};	
+		Integer[] sorted = MergeSort.sort(arr);
+		Integer[] expected = {-2, 2, 3, 3, 3, 4, 5, 6, 6, 8, 10, 11, 123, 123};
+		assertTrue(Arrays.equals(sorted, expected));
+	}
 }
