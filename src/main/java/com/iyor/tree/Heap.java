d7c0d5aafb43a5be9fd6d94fb49c28c16e4219b5
Erik <erikejan@hotmail.com>
2017-01-26 11:06:23 +0100

    Heep.add implemented but untested

--- src/main/java/com/iyor/tree/Heap.java
+++ src/main/java/com/iyor/tree/Heap.java
@@ -7,6 +7,9 @@ public class Heap<E extends Comparable<E>> {
 	ArrayList<E> data = new ArrayList<E>();
 
 	public boolean add(E e) {
+		data.add(e);
+		int currentIdx = data.size();
+		reposition(currentIdx);
 		return true;				
 	}
 
@@ -16,5 +19,20 @@ public class Heap<E extends Comparable<E>> {
 
 	public boolean contains(E e) {
 		return true;				
+	}
+
+	private void reposition(int index) {
+		int parent = findParent(index);
+		if (data.get(index).compareTo(data.get(parent))>0) {
+			return;
+		}
+		E temp = data.get(parent);
+		data.set(parent, data.get(index));
+		data.set(index, temp);
+		reposition(parent);
+	}
+
+	private int findParent(int childIdx) {
+		return (childIdx-1)/2;	
 	}
 }
